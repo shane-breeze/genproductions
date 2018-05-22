2ef832d71bf3600fe84336ac5de6ba688e8be014
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-05-22 19:57:51 +0100

    Don't use the gridpack_generation.sh's log file

--- bin/MadGraph5_aMCatNLO/gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -354,10 +354,11 @@ if [ ! -d ${AFS_GEN_FOLDER}/${name}_gridpack ]; then
   sed -i '$ a display multiparticles' ${name}_proc_card.dat
   ./$MGBASEDIRORIG/bin/mg5_aMC ${name}_proc_card.dat
  
-  is5FlavorScheme=0
-  if tail -n 20 $LOGFILE | grep -q -e "^p *=.*b\~.*b" -e "^p *=.*b.*b\~"; then 
-    is5FlavorScheme=1
-  fi
+  #is5FlavorScheme=0
+  #if tail -n 20 $LOGFILE | grep -q -e "^p *=.*b\~.*b" -e "^p *=.*b.*b\~"; then 
+  #  is5FlavorScheme=1
+  #fi
+  is5FlavourScheme=1 # my models are always 5f
 
   #*FIXME* workaround for broken set cluster_queue handling
   if [ "$queue" == "condor" ]; then
@@ -390,13 +391,14 @@ elif [ "${jobstep}" = "INTEGRATE" ] || [ "${jobstep}" = "ALL" ]; then
   echo "Reusing existing directory assuming generated code already exists"
   echo "WARNING: If you changed the process card you need to clean the folder and run from scratch"
 
-  if [ "$is5FlavorScheme" -eq -1 ]; then
-    if cat $LOGFILE_NAME*.log | grep -q -e "^p *=.*b\~.*b" -e "^p *=.*b.*b\~"; then 
-        is5FlavorScheme=1
-    else
-        is5FlavorScheme=0
-    fi 
-  fi
+  #if [ "$is5FlavorScheme" -eq -1 ]; then
+  #  if cat $LOGFILE_NAME*.log | grep -q -e "^p *=.*b\~.*b" -e "^p *=.*b.*b\~"; then 
+  #      is5FlavorScheme=1
+  #  else
+  #      is5FlavorScheme=0
+  #  fi 
+  #fi
+  is5FlavorScheme=1
   
   cd $AFS_GEN_FOLDER
   
@@ -743,11 +745,11 @@ if [ -e $CARDSDIR/${name}_externaltarball.dat ]; then
 fi
 
 
-echo "Saving log file(s)"
-#copy log file
-for i in ${LOGFILE_NAME}*.log; do 
-    cp $i ${i/$LOGFILE_NAME/gridpack_generation}; 
-done
+# echo "Saving log file(s)"
+# #copy log file
+# for i in ${LOGFILE_NAME}*.log; do 
+#     cp $i ${i/$LOGFILE_NAME/gridpack_generation}; 
+# done
 
 
 
